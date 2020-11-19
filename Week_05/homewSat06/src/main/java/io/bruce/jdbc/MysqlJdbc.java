83cb2698f44fef9267523226a74cbfe0c2631b19
bruce <bruce.du@xiaoxiongyouxi.com>
2020-11-20 00:22:31 +0800

    第五周作业补充

--- Week_05/homewSat06/src/main/java/io/bruce/jdbc/MysqlJdbc.java
+++ Week_05/homewSat06/src/main/java/io/bruce/jdbc/MysqlJdbc.java
@@ -1,9 +1,7 @@
 package io.bruce.jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.*;
 
 public class MysqlJdbc {
 
@@ -33,15 +31,99 @@ public class MysqlJdbc {
         }
     }
 
-    public static void main(String[] args) {
+    public boolean insert(String table, String column, List<Object> values) {
+        try {
+            String insertTemplate = buildInsertSqlTemplate(table, column, values.size());
+            preparedStatement = connection.prepareStatement(insertTemplate);
+            for (int i=1; i<values.size() + 1; i++) {
+                preparedStatement.setObject(i, values.get(i-1));
+            }
+            System.out.println(preparedStatement.toString());
 
-
-
+            preparedStatement.execute();
+            System.out.println("Insert successfully");
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
-    public int insert(){
+    private String buildInsertSqlTemplate(String table, String column, int valueAmount) {
+        String s = "";
+        for (int i = 0; i < valueAmount - 1; i++) {
+            s += ",?";
+        }
+        return "insert into " + table + " " + column + " values (?" +
+                s +
+                ")";
+    }
 
+    public List<Map<String, Object>> query(String table, Map<String, Object> values, String condition) {
+        try {
+            String sqlTemplate = buildQuerySqlTemplate(table, values, condition);
 
+            preparedStatement = connection.prepareStatement(sqlTemplate);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<Map<String, Object>> list = new ArrayList<>();
+            while (resultSet.next()) {
+                for (String key: values.keySet()) {
+                    values.put(key, resultSet.getObject(key));
+                }
+                list.add(new HashMap<>(values));
+            }
+            return list;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String buildQuerySqlTemplate(String table, Map<String, Object> values, String condition) {
+        String sqlTemplate = "select " + values.keySet().toString().substring(1,
+                values.keySet().toString().length()-1) + " from " + table;
+        if (condition != null) {
+            sqlTemplate += " where " + condition;
+        }
+        return sqlTemplate;
+    }
+
+    private void close() throws SQLException {
+        preparedStatement.close();
+        connection.close();
+        System.out.println("Connection close");
+    }
+
+    public static void main(String[] args) {
+        Map<String, Object> valuesMap = new HashMap<>();
+        valuesMap.put("id", 0);
+        valuesMap.put("name", "name");
+        valuesMap.put("password", "password");
+        valuesMap.put("phoneNumber", "phoneNumber");
+        valuesMap.put("money", 0);
+        System.out.println(valuesMap.keySet().toString().substring(1, valuesMap.keySet().toString().length()-1));
+
+        try {
+            MysqlJdbc mysqlJdbc = new MysqlJdbc();
+            mysqlJdbc.createConnection();
+
+            String table = "users";
+            String columns = "(name, password, phoneNumber, money)";
+            List<Object> values = Arrays.asList("name", "password", "phoneNumber", 0);
+
+            mysqlJdbc.insert(table, columns, values);
+
+            List<Map<String, Object>> results = mysqlJdbc.query(table, valuesMap, null);
+            for (Map r: results) {
+                System.out.println(r.toString());
+            }
+
+            mysqlJdbc.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
